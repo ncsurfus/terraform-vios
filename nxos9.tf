36c0c7d8e813853ef27bf56e86d10b50e718cf38
Nathan Surfus <ncsurfus@gmail.com>
2018-11-17 09:19:55 -0600

    Started moving nx07 to nx09 since 07 has some issues

--- nxos9.tf
+++ nxos9.tf
@@ -1,22 +1,22 @@
 
-resource "libvirt_volume" "nxos7" {
-  name = "nxos7"
-  source = "backing_images/titanium-final.7.3.0.D1.1.qcow2"
+resource "libvirt_volume" "nxos9" {
+  name = "nxos9"
+  source = "backing_images/nxosv-final.7.0.3.I7.2.qcow2"
 }
 
-resource "libvirt_volume" "nxos7-1" {
-  name = "nxos7-1"
-  source = "${libvirt_volume.nxos7.id}"
+resource "libvirt_volume" "nxos9-1" {
+  name = "nxos9-1"
+  source = "${libvirt_volume.nxos9.id}"
 
   provisioner "local-exec" {
     command = "guestfish add ${self.id} : run : mount /dev/sda4 / : upload $(pwd)/configs/${self.name}.txt /nxos_config.txt : cat /nxos_config.txt : quit"
   }
 }
 
-resource "libvirt_domain" "nxos7-1" {
-  name = "nxos7-1"
-  memory = "4096"
-  vcpu = 4
+resource "libvirt_domain" "nxos9-1" {
+  name = "nxos9-1"
+  memory = "8192"
+  vcpu = 2
 
   network_interface {
     network_name = "vm_network"
