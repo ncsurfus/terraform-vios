a92d5dada425b8afb48714a8d55a569cecb46eed
Nathan Surfus <ncsurfus@gmail.com>
2018-11-17 10:58:32 -0600

    Added cleanup to quickly fix issues when terraform apply goes bad

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+virsh undefine sw-1
+virsh destroy nxos9-1
+virsh destroy rtr-1
+virsh destroy sw-1
+virsh undefine nxos9-1
+virsh undefine rtr-1
+virsh undefine sw-1
+virsh net-destroy vm_network
+virsh net-undefine vm_network
+virsh vol-delete nxos9 --pool default
+virsh vol-delete nxos9-1 --pool default
+virsh vol-delete rtr-1 --pool default
+virsh vol-delete sw-1 --pool default
+virsh vol-delete vios-l2 --pool default
+virsh vol-delete vios-l3 --pool default
